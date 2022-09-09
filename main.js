8b7f349831cfa6465c1bd0d696555f9e64e33c31
Wilton de Oliveira <104309027+Wilton21y@users.noreply.github.com>
2022-09-09 18:46:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const sobre = document.querySelector('.sobre').addEventListener('click', function exibir() {
+  document.querySelector('.txt').classList.toggle('visivel')
+});
+
+const tec = document.querySelector('.tec').addEventListener('click', function exibir_tec() {
+  document.querySelector('.txtTec').classList.toggle('visivel')
+}
+);
